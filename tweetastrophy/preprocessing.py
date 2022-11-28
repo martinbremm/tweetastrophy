6f6fce59d82f55c5ae8d91b63c9ea62f0ff7a88b
Damla Cidam Kartal <damlacidam@gmail.com>
2022-11-28 17:48:56 +0100

    getting and cleaning data

--- tweetastrophy/preprocessing.py
+++ tweetastrophy/preprocessing.py
@@ -1,10 +1,6 @@
 import string
 import re
 
-from tweetastrophy.get_data import get_data
-
-train_data, test_data = get_data()
-
 ## KEYWORD HANDLING
 
 def preprocessing(train_data):
@@ -16,19 +12,17 @@ def preprocessing(train_data):
     train_data["keyword"] = train_data['keyword'].str.replace("%20", " ")
 
     # adding hashtags to keywords
-    train_data["keyword"] + train_data["text"].apply(lambda x: " ".join(re.findall("#(\w+)", x)).lower())
-
-    #cleaning urls
-
-    train_data['text'] = train_data['text'].apply(lambda x: re.sub('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*',
-                                                       '', x))
-
+    train_data['keyword'] = train_data["keyword"] + train_data["text"].apply(lambda x: " ".join(re.findall("#(\w+)", x)).lower())
 
     ### TEXT
     train_data['text'] = train_data['text'].apply(lambda x: x.lower())
 
     #strip data
     train_data['text'] = train_data['text'].str.strip()
+
+    #cleaning urls
+    train_data['text'] = train_data['text'].apply(lambda x: re.sub('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*',
+                                                       '', x))
 
     #remove digits
     train_data['text'] = train_data['text'].apply(lambda x: ''.join(i for i in x if i not in string.digits))
