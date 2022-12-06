397cf0ac64ad337e6d64ddf779ea5b9bf465550e
Damla Cidam Kartal <damlacidam@gmail.com>
2022-12-06 15:04:14 +0100

    changed design of the map

--- tweetastrophy/app.py
+++ tweetastrophy/app.py
@@ -58,7 +58,7 @@ text_archive = list(set(text_archive))
 
 if txt == "":
     map = folium.Map(location=[0,0],
-                 tiles="Mapbox Bright",
+                 tiles="cartodbpositron",
                  zoom_start=3, control_scale=True)
 
     text_archive.remove(txt)
