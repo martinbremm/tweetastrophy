304f151a3f86dc8d89bc8fa71a7921deeb5a6843
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-07 10:11:44 +0100

    putting map into a separate function in map.py and adding cache to it

--- tweetastrophy/app.py
+++ tweetastrophy/app.py
@@ -1,10 +1,8 @@
-import pandas as pd
 import streamlit as st
-import folium
 from streamlit_folium import st_folium
-
-from location import create_location
 from predict import get_prediction
+from map import create_map
+
 
 text_archive = []
 
@@ -53,86 +51,13 @@ def local_css(file_name):
 local_css("tweetastrophy/config.toml")
 
 
-
-
-# creating location df
+# creating text archive of all the txts
 text_archive.append(txt)
 
 text_archive = list(set(text_archive))
 
-if txt == "":
-    map = folium.Map(location=[0,0],
-                 tiles="cartodbpositron",
-                 zoom_start=3, control_scale=True)
-
-    text_archive.remove(txt)
-
-else:
-
-    text_df = pd.DataFrame.from_dict(data={"text": text_archive})
-
-    # looping over text entries in text archive and extracting locations
-    locations_df = create_location(text_df)
-
-
-    # creating basic map in folium
-    map = folium.Map(location=[locations_df.lat.mean(), locations_df.lon.mean()],
-                    tiles="cartodbpositron",
-                    zoom_start=5, min_zoom=3, control_scale=True)
-
-    # mapping circles to df in DataFrame
-    df_dict = locations_df.to_dict("records")
-
-    dictionary_list = []
-
-    for row in df_dict:
-
-        # determining the color of the circle
-        if prediction == "The tweet is Disaster Tweet":
-            color = "#EE4B2B" # red
-        else:
-            color = "#008000" # green
-
-        # checking for rows without coordinates
-        if row["lat"] == 0.0 or row["lon"] == 0.0:
-            continue
-
-        # city data available
-        elif row["city"] != "Unknown":
-            if row["size"] == "Not Found":
-                radius=10000
-            else:
-                radius=row["size"]
-
-            folium.Circle(location=[row["lat"], row["lon"]], radius=radius, popup=row["city"],
-                                color=color, fill=True, fill_color=color).add_to(map) # red
-
-        # region data available
-        elif (row["region"] != "Unknown") & (row["city"] == "Unknown"):
-            if row["size"] == "Not Found":
-                radius=660000
-            else:
-                radius=row["size"]
-
-            folium.Circle(location=[row["lat"], row["lon"]], radius=radius, popup=row["region"],
-                                color=color, fill=True, fill_color=color).add_to(map) # green
-
-        # country data available
-        elif (row["country"] != "Unknown") & (row["region"] == "Unknown") & (row["city"] == "Unknown"):
-            if row["size"] == "Not Found":
-                radius=660000
-            else:
-                radius=row["size"]
-
-            folium.Circle(location=[row["lat"], row["lon"]], radius=660000, popup=row["country"],
-                                color=color, fill=True, fill_color=color).add_to(map) # blue
-
-    # adding automatic zoom to last df
-    sw = locations_df[['lat', 'lon']].min().values.tolist()
-    ne = locations_df[['lat', 'lon']].max().values.tolist()
-
-    map.fit_bounds([sw, ne], padding=(1,1), max_zoom=10)
-
+# adding map based on the previous texts the person has entered
+map = create_map(text_archive, prediction)
 
 # render Folium map in Streamlit
 st_data = st_folium(map, width=2000, height=600)
---
+++ tweetastrophy/map.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import streamlit as st
+import folium
+
+from location import create_location
+
+@st.cache()
+def create_map(text_archive, prediction):
+    # creating location df
+    if text_archive[-1] == "":
+        map = folium.Map(location=[0,0],
+                    tiles="cartodbpositron",
+                    zoom_start=3, control_scale=True)
+
+        text_archive.remove("")
+
+    else:
+
+        text_df = pd.DataFrame.from_dict(data={"text": text_archive})
+
+        # looping over text entries in text archive and extracting locations
+        locations_df = create_location(text_df)
+
+
+        # creating basic map in folium
+        map = folium.Map(location=[locations_df.lat.mean(), locations_df.lon.mean()],
+                        tiles="cartodbpositron",
+                        zoom_start=5, min_zoom=3, control_scale=True)
+
+        # mapping circles to df in DataFrame
+        df_dict = locations_df.to_dict("records")
+
+        for row in df_dict:
+
+            # determining the color of the circle
+            if prediction == "The tweet is Disaster Tweet":
+                color = "#EE4B2B" # red
+            else:
+                color = "#008000" # green
+
+            # checking for rows without coordinates
+            if row["lat"] == 0.0 or row["lon"] == 0.0:
+                continue
+
+            # city data available
+            elif row["city"] != "Unknown":
+                if row["size"] == "Not Found":
+                    radius=10000
+                else:
+                    radius=row["size"]
+
+                folium.Circle(location=[row["lat"], row["lon"]], radius=radius, popup=row["city"],
+                                    color=color, fill=True, fill_color=color).add_to(map) # red
+
+            # region data available
+            elif (row["region"] != "Unknown") & (row["city"] == "Unknown"):
+                if row["size"] == "Not Found":
+                    radius=660000
+                else:
+                    radius=row["size"]
+
+                folium.Circle(location=[row["lat"], row["lon"]], radius=radius, popup=row["region"],
+                                    color=color, fill=True, fill_color=color).add_to(map) # green
+
+            # country data available
+            elif (row["country"] != "Unknown") & (row["region"] == "Unknown") & (row["city"] == "Unknown"):
+                if row["size"] == "Not Found":
+                    radius=660000
+                else:
+                    radius=row["size"]
+
+                folium.Circle(location=[row["lat"], row["lon"]], radius=660000, popup=row["country"],
+                                    color=color, fill=True, fill_color=color).add_to(map) # blue
+
+        # adding automatic zoom to last df
+        sw = locations_df[['lat', 'lon']].min().values.tolist()
+        ne = locations_df[['lat', 'lon']].max().values.tolist()
+
+        map.fit_bounds([sw, ne], padding=(1,1), max_zoom=10)
+
+        return map
