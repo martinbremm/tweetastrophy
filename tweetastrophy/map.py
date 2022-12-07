ed3be74520fde7d203b202b207812ba34bc67336
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-07 16:36:36 +0100

    changed caching function

--- tweetastrophy/map.py
+++ tweetastrophy/map.py
@@ -6,7 +6,7 @@ from streamlit_folium import st_folium
 
 from location import create_location
 
-@st.cache(persist=True, allow_output_mutation=True, suppress_st_warning=True)
+@st.experimental_memo(suppress_st_warning=True)
 def create_map(text_archive, prediction):
     # creating location df
     if text_archive[-1] == "":
