520b3e9eb09a02a5da748f1602d27fac17e37a8e
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-07 11:10:54 +0100

    allowing output mutations

--- tweetastrophy/map.py
+++ tweetastrophy/map.py
@@ -4,7 +4,7 @@ import folium
 
 from location import create_location
 
-@st.cache(hash_funcs={'builtins.function': hash})
+@st.cache(hash_funcs={'builtins.function': hash}, allow_output_mutation=True)
 def create_map(text_archive, prediction):
     # creating location df
     if text_archive[-1] == "":
