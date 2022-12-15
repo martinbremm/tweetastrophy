ee285665e0ba4b1e132ea1a0694dcc4abd5eefcd
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-15 21:24:26 +0100

    adding comments to app.py

--- tweetastrophy/app.py
+++ tweetastrophy/app.py
@@ -6,16 +6,14 @@ from map import create_map
 from preprocessing import text_preprocessing
 
 
+# configuring streamlit layout
 st.set_page_config(page_title='Tweetastrophy', page_icon=':tada:', layout='wide')
 
-
-
+# initializing session state
 if 'txt' not in st.session_state:
     st.session_state['txt'] = []
 if 'pred' not in st.session_state:
     st.session_state['pred'] = []
-
-
 
 # frontend style descriptors
 hide_menu = """
@@ -46,7 +44,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-
+# logo and text input in sidebar
 with st.sidebar:
     a, b = st.columns([1, 20])
     with a:
@@ -62,17 +60,14 @@ with st.sidebar:
         txt = st.text_area('', placeholder='. . .')
         st.button('Predict')
 
-# creating prediction value
-prediction = get_prediction(txt)
-
+# creating prediction container
 prediction_container = st.container()
 col1, col2 = st.columns([8,17])
 
 # creating prediction value
 prediction = get_prediction(txt)
 
-
-
+# css config
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(hide_menu, unsafe_allow_html=True)
@@ -87,17 +82,16 @@ st.session_state['pred'] = prediction
 
 st.write(st.session_state['pred'])
 
-
+# prediction output
 if prediction == 'The tweet is Disaster Tweet':
     with col1:
         st.markdown('<p class="big-font"> Tweet is a disaster &#9888;&#65039; </p>', unsafe_allow_html=True)
-
 
 elif prediction == 'The tweet is Non Disaster Tweet':
     with col1:
         st.markdown('<p class="big-font"> Tweet is not a disaster &#x2705;</p>', unsafe_allow_html=True)
 
-
+# map creation
 if txt == "elon musk":
     st.image("tweetastrophy/elon_meme.png")
 
@@ -109,10 +103,11 @@ else:
     st.markdown('<p class="big-font">Waiting for your tweet.. &#128564; </p>', unsafe_allow_html=True)
     create_map(st.session_state['txt'], st.session_state['pred'])
 
-
+# clearing invalid entries from session state
 if "" in st.session_state['txt']:
     st.session_state['txt'].remove("")
 if "" in st.session_state['pred']:
     st.session_state['pred'].remove("")
 
+# clearing streamlit cache
 #create_map.clear()
