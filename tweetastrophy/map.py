869f183c1cce7c8b264edc4b92e0620f2959ad90
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-15 21:33:02 +0100

    new intialize map

--- tweetastrophy/map.py
+++ tweetastrophy/map.py
@@ -11,12 +11,16 @@ from location import create_location
 def create_map(text_archive, prediction):
 
     # initialize empty map if no input or no previous input
-    if (text_archive == "") | (text_archive[-1] == ""):
-        map = folium.Map(location=[0,0],
+    map = folium.Map(location=[0,0],
                     tiles="cartodbpositron",
                     zoom_start=3, control_scale=True)
 
+    if not text_archive:
         return  st_folium(map, width=1200, height=600, returned_objects=[])
+
+    elif type(text_archive) == list:
+        if text_archive[-1] == "":
+            return  st_folium(map, width=1200, height=600, returned_objects=[])
 
     else:
         text_df = pd.DataFrame.from_dict(data={"text": text_archive})
