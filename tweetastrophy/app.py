79fa1bf1f9cde084cde15ded2c6737e7820d9fb9
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-18 18:30:56 +0100

    test

--- tweetastrophy/app.py
+++ tweetastrophy/app.py
@@ -8,74 +8,74 @@ from preprocessing import text_preprocessing
 # configuring streamlit layout
 st.set_page_config(page_title='Tweetastrophy', page_icon=':tada:', layout='wide')
 
+
+# initializing session state
+if 'txt' not in st.session_state:
+    st.session_state['txt'] = []
+if 'pred' not in st.session_state:
+    st.session_state['pred'] = []
+
+# frontend style descriptors
+hide_menu = """
+<style>
+#MainMenu {
+    visibility:hidden;
+}
+
+footer {
+    visibility:hidden;
+}
+</style>
+"""
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:30px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+.mid-font {
+    font-size:20px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# logo and text input in sidebar
+with st.sidebar:
+    a, b = st.columns([1, 20])
+    with a:
+        st.text("")
+        st.image("tweetastrophy/twitter_logo.png", width=50)
+    with b:
+        st.title("Tweetastrophy")
+
+    st.text("")
+    with st.container() :
+        #st.markdown('<p class="mid-font"> Enter your tweet here 👇🏼 !!</p>', unsafe_allow_html=True)
+        st.info('Enter your tweet here 👇🏼 !!')
+        txt = st.text_area('', placeholder='. . .')
+        st.button('Predict')
+
+# creating prediction container
+prediction_container = st.container()
+col1, col2 = st.columns([8,17])
+
+# creating prediction value
+prediction = get_prediction(txt)
+
+# css config
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(hide_menu, unsafe_allow_html=True)
+
+local_css("tweetastrophy/config.toml")
+
 @st.experimental_memo(suppress_st_warning=True)
 def mapping_app():
-
-    # initializing session state
-    if 'txt' not in st.session_state:
-        st.session_state['txt'] = []
-    if 'pred' not in st.session_state:
-        st.session_state['pred'] = []
-
-    # frontend style descriptors
-    hide_menu = """
-    <style>
-    #MainMenu {
-        visibility:hidden;
-    }
-
-    footer {
-        visibility:hidden;
-    }
-    </style>
-    """
-
-    st.markdown("""
-    <style>
-    .big-font {
-        font-size:30px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-    st.markdown("""
-    <style>
-    .mid-font {
-        font-size:20px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-    # logo and text input in sidebar
-    with st.sidebar:
-        a, b = st.columns([1, 20])
-        with a:
-            st.text("")
-            st.image("tweetastrophy/twitter_logo.png", width=50)
-        with b:
-            st.title("Tweetastrophy")
-
-        st.text("")
-        with st.container() :
-            #st.markdown('<p class="mid-font"> Enter your tweet here 👇🏼 !!</p>', unsafe_allow_html=True)
-            st.info('Enter your tweet here 👇🏼 !!')
-            txt = st.text_area('', placeholder='. . .')
-            st.button('Predict')
-
-    # creating prediction container
-    prediction_container = st.container()
-    col1, col2 = st.columns([8,17])
-
-    # creating prediction value
-    prediction = get_prediction(txt)
-
-    # css config
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(hide_menu, unsafe_allow_html=True)
-
-    local_css("tweetastrophy/config.toml")
-
     # preprocessing text
     txt = text_preprocessing(txt)
 
