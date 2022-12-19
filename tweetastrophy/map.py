e18f5d40844256bc084da24bec138de88512af26
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-19 09:47:10 +0100

    not showing spinner

--- tweetastrophy/map.py
+++ tweetastrophy/map.py
@@ -7,7 +7,7 @@ from preprocessing import text_preprocessing
 
 from location import create_location
 
-@st.experimental_memo(suppress_st_warning=True)
+@st.experimental_memo(suppress_st_warning=True, show_spinner=False)
 def create_map(text_archive, prediction):
 
     # initialize empty map if no input or no previous input
