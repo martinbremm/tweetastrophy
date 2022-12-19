7c8f66df31a4387b1763e7d5f112b2c8154ad9b4
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-19 10:08:43 +0100

    old cache

--- tweetastrophy/map.py
+++ tweetastrophy/map.py
@@ -7,7 +7,7 @@ from preprocessing import text_preprocessing
 
 from location import create_location
 
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+@st.experimental_memo(suppress_st_warning=True)
 def create_map(text_archive, prediction):
 
     # initialize empty map if no input or no previous input
