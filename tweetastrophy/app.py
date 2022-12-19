d99d0130327cd50c85df197639a8705f173f5991
martin bremm <m.bremm@student.maastrichtuniversity.nl>
2022-12-19 09:15:02 +0100

    separating single value state and list state

--- tweetastrophy/app.py
+++ tweetastrophy/app.py
@@ -63,16 +63,16 @@ with st.sidebar:
         st.info('Enter your tweet here 👇🏼 !!')
         txt = st.text_area(label='', placeholder='. . .', key='txt')
 
-    # creating prediction value
-    prediction = get_prediction(txt)
-    st.session_state['pred'] = prediction
-
     # preprocessing text
     txt = text_preprocessing(txt)
 
     st.write(st.session_state)
 
-    st.button(label='Predict', on_click = updating_session_state)
+    st.session_state['pred'] = st.button(label='Predict', on_click = get_prediction, args=(txt, ))
+
+    st.session_state['texts'].append(st.session_state['txt'])
+    st.session_state['predictions'].append(st.session_state['pred'])
+
 
     st.write(st.session_state)
 
@@ -88,11 +88,11 @@ def local_css(file_name):
 local_css("tweetastrophy/config.toml")
 
 # prediction output
-if prediction == 'The tweet is Disaster Tweet':
+if st.session_state['pred'] == 'The tweet is Disaster Tweet':
     with col1:
         st.markdown('<p class="big-font"> Tweet refers to real disaster &#9888;&#65039; </p>', unsafe_allow_html=True)
 
-elif prediction == 'The tweet is Non Disaster Tweet':
+elif st.session_state['pred'] == 'The tweet is Non Disaster Tweet':
     with col1:
         st.markdown('<p class="big-font"> Tweet does not refer to a disaster &#x2705;</p>', unsafe_allow_html=True)
 
